98f32e714e43b51266dc0c6eb5869908f8c733d0
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2021-03-26 11:08:11 +0530

    First Commit

---
+++ JwtAuthenticationApi/Repository/AuthenticationManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthenticationApi.Repository
+{
+    public class AuthenticationManager : IAuthenticationManager
+    {
+        private readonly Dictionary<string, string> users = new Dictionary<string, string>() {
+            { "abc","abc@123"},
+            {"test","test@123" }
+        };
+
+        private readonly string tokenKey;
+        public AuthenticationManager(string tokenKey)
+        {
+            this.tokenKey = tokenKey;
+        }
+        public string Authenticate(string username, string password)
+        {
+            if (!users.Any(u => u.Key == username && u.Value == password))
+            {
+                return null;
+            }
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(tokenKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
